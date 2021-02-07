85f617a11728d64d0bb750c50ced2930aa4417f2
Sereno Mendes Nascimento <sereno.desenvolvimento@gmail.com>
2021-02-07 08:18:51 -0300

    Added some documentation explaining the need of this abstract class over the Models

--- src/main/java/jaya/currencyconverter/entity/Entity.java
+++ src/main/java/jaya/currencyconverter/entity/Entity.java
@@ -1,5 +1,11 @@
 package jaya.currencyconverter.entity;
 
+/**
+ * This class was created to help database tables creation.
+ * When the DataStorageModule starts db configuration,
+ * we itarate over a set of entities (every model extends Entity)
+ * and try create the table. 
+ */
 public abstract class Entity {
     
 }
