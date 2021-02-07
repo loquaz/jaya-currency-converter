5ba587dba32794d8b085442d2dc0ef420a565ea8
Sereno Mendes Nascimento <sereno.desenvolvimento@gmail.com>
2021-02-07 20:56:20 -0300

    Added more error checks in the unit tests for the conversion functionality

--- src/test/java/jaya/currencyconverter/ConversionUnitTest.java
+++ src/test/java/jaya/currencyconverter/ConversionUnitTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.sql.SQLException;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -22,6 +23,7 @@ import jaya.currencyconverter.controller.CurrencyConversionController;
 import jaya.currencyconverter.controller.UserController;
 import jaya.currencyconverter.dto.TransactionDTO;
 import jaya.currencyconverter.entity.User;
+import jaya.currencyconverter.repository.UserRepository;
 
 @TestMethodOrder(OrderAnnotation.class)
 public class ConversionUnitTest {
@@ -30,12 +32,23 @@ public class ConversionUnitTest {
     private static Context userctx;
     private static CurrencyConversionController controller;
     private static UserController userController;
+    private static User user;
+    private static UserRepository userRepo;
     
     @BeforeAll
     static public void boot(){
         Injector injector = Guice.createInjector( ServerModule.test() );
         controller = injector.getInstance(CurrencyConversionController.class);
         userController = injector.getInstance(UserController.class);
+        userRepo = injector.getInstance(UserRepository.class);
+
+        /* sign up an user */
+        user = new User("sereno", "123");
+        try{
+            userRepo.saveUser(user);
+        }catch(SQLException e){
+
+        }
     }
 
     @BeforeEach
@@ -47,12 +60,6 @@ public class ConversionUnitTest {
     @Test
     @Order(1)
     public void POST_create_a_valid_transaction(){
-
-        /* sign up an user */
-        User user = new User("sereno", "123");
-        when(userctx.bodyAsClass(User.class)).thenReturn(user);
-        userController.signup(userctx);
-        verify(userctx).status(201);
 
         /*
          * A Valid transaction must have the following fields:
@@ -72,6 +79,70 @@ public class ConversionUnitTest {
         when(ctx.bodyAsClass(TransactionDTO.class)).thenReturn( transaction );
         controller.doConversion(ctx);
         verify(ctx).status(201);
+
+    }
+
+    @Test
+    @Order(2)
+    public void POST_do_not_create_n_transaction_without_currency_from(){
+
+        TransactionDTO transaction = new TransactionDTO();
+        transaction.setUserID(user.getId());
+        transaction.setCurrencyFrom( null ); //invalid value
+        transaction.setCurrencyTo("USD");
+        transaction.setAmount(new BigDecimal(53));
+        
+        when(ctx.bodyAsClass(TransactionDTO.class)).thenReturn( transaction );
+        controller.doConversion(ctx);
+        verify(ctx).status(400);
+
+    }
+
+    @Test
+    @Order(3)
+    public void POST_do_not_create_n_transaction_without_currency_to(){
+
+        TransactionDTO transaction = new TransactionDTO();
+        transaction.setUserID(user.getId());
+        transaction.setCurrencyFrom( "USD" ); 
+        transaction.setCurrencyTo(""); //invalid value
+        transaction.setAmount(new BigDecimal(53));
+        
+        when(ctx.bodyAsClass(TransactionDTO.class)).thenReturn( transaction );
+        controller.doConversion(ctx);
+        verify(ctx).status(400);
+
+    }
+
+    @Test
+    @Order(4)
+    public void POST_do_not_create_a_transaction_with_invalid_user_id(){
+
+        TransactionDTO transaction = new TransactionDTO();
+        transaction.setUserID( 2 );
+        transaction.setCurrencyFrom( "EUR" ); 
+        transaction.setCurrencyTo("BRL");
+        transaction.setAmount(new BigDecimal(53));
+        
+        when(ctx.bodyAsClass(TransactionDTO.class)).thenReturn( transaction );
+        controller.doConversion(ctx);
+        verify(ctx).status(400);
+
+    }
+
+    @Test
+    @Order(5)
+    public void POST_do_not_create_a_transaction_without_amount(){
+
+        TransactionDTO transaction = new TransactionDTO();
+        transaction.setUserID( user.getId() );
+        transaction.setCurrencyFrom( "EUR" ); 
+        transaction.setCurrencyTo("BRL");
+        transaction.setAmount( null ); // invalid amount
+        
+        when(ctx.bodyAsClass(TransactionDTO.class)).thenReturn( transaction );
+        controller.doConversion(ctx);
+        verify(ctx).status(400);
 
     }
     
