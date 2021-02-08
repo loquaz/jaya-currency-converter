36750499464030d8ee49c4a26fc9ab518ff00d30
Sereno Mendes Nascimento <sereno.desenvolvimento@gmail.com>
2021-02-08 14:16:36 -0300

    Added startup method

--- src/test/java/jaya/currencyconverter/IntegrationTest.java
+++ src/test/java/jaya/currencyconverter/IntegrationTest.java
@@ -2,6 +2,8 @@ package jaya.currencyconverter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.sql.SQLException;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
@@ -10,16 +12,38 @@ import org.junit.jupiter.api.Test;
 
 import io.javalin.Javalin;
 import jaya.currencyconverter.config.module.ServerModule;
+import jaya.currencyconverter.controller.CurrencyConversionController;
+import jaya.currencyconverter.controller.UserController;
+import jaya.currencyconverter.entity.User;
+import jaya.currencyconverter.repository.UserRepository;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 
 public class IntegrationTest {
     
-    private Injector injector   = Guice.createInjector( ServerModule.test() );
-    private Javalin app         = injector.getInstance(WebEntryPoint.class).boot();
+    private static Injector injector;
+    private static Javalin app;
+    private static CurrencyConversionController controller;
+    private static UserController userController;
+    private static UserRepository userRepo;
+    private static User user;
 
     @BeforeAll
-    
+    static public void boot(){
+        injector = Guice.createInjector( ServerModule.test() );
+        app         = injector.getInstance(WebEntryPoint.class).boot();
+        controller = injector.getInstance(CurrencyConversionController.class);
+        userController = injector.getInstance(UserController.class);
+        userRepo = injector.getInstance(UserRepository.class);
+
+        /* sign up an user */
+        user = new User("sereno", "123");
+        try{
+            userRepo.saveUser(user);
+        }catch(SQLException e){
+
+        }
+    }
 
     @Test
     public void GET_fetch_transactions_returns_valid_list(){
