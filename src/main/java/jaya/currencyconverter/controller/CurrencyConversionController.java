d45803bf054c00e2bdd828324f1178a946ae61d8
Sereno Mendes Nascimento <sereno.desenvolvimento@gmail.com>
2021-02-07 22:32:31 -0300

    Refactorying to use the new data http response field and added listTransactionByUser new route

--- src/main/java/jaya/currencyconverter/controller/CurrencyConversionController.java
+++ src/main/java/jaya/currencyconverter/controller/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package jaya.currencyconverter.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -9,6 +11,7 @@ import io.javalin.http.Context;
 import jaya.currencyconverter.config.module.CurrencyConversionAnnotations.CurrencyConversionControllerLogger;
 import jaya.currencyconverter.dto.HttpResponseDTO;
 import jaya.currencyconverter.dto.TransactionDTO;
+import jaya.currencyconverter.entity.CurrencyConversionTransaction;
 import jaya.currencyconverter.service.CurrencyConversionService;
 
 @Singleton
@@ -34,11 +37,28 @@ public class CurrencyConversionController {
             TransactionDTO transaction  = ctx.bodyAsClass(TransactionDTO.class);        
             TransactionDTO result       = this.service.createTransaction(transaction);
             ctx.status(201);
-            ctx.json(result);
+            ctx.json( new HttpResponseDTO(201, "currency conversion result", false, result) );
 		} catch (Exception e) {
             this.currencyConversionLogger.error(e.getMessage());
             ctx.status(400);
-            ctx.json(new HttpResponseDTO(400, e.getMessage(),true));            
+            ctx.json(new HttpResponseDTO(400, e.getMessage(),true, null));            
+		}
+    }
+
+     /**
+     * List all user transaction
+     */
+    public void listTransactionByUser(Context ctx)  {
+ 
+        try {
+            int userID = Integer.parseInt(ctx.queryParam("userID"));
+            List<TransactionDTO> transactions = this.service.getAllUserTransactions(userID);
+            ctx.status(200);
+            ctx.json( new HttpResponseDTO(200, "user transactions", false, transactions) );
+		} catch (Exception e) {
+            this.currencyConversionLogger.error(e.getMessage());
+            ctx.status(400);
+            ctx.json(new HttpResponseDTO(400, e.getMessage(),true, null));            
 		}
     }
 
