c081cb9ab3d4b8bcfe72faaa7a6978b076a61cc9
Sereno Mendes Nascimento <sereno.desenvolvimento@gmail.com>
2021-02-08 13:49:18 -0300

    Added integration tests

---
+++ src/test/java/jaya/currencyconverter/IntegrationTest.java
@@ -0,0 +1,27 @@
+package jaya.currencyconverter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import org.junit.jupiter.api.Test;
+
+import io.javalin.Javalin;
+import jaya.currencyconverter.config.module.ServerModule;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+public class IntegrationTest {
+    
+    private Injector injector   = Guice.createInjector( ServerModule.test() );
+    private Javalin app         = injector.getInstance(WebEntryPoint.class).boot();
+
+    @Test
+    public void GET_fetch_transactions_returns_valid_list(){
+
+        HttpResponse res = Unirest.get("http://localhost:5000/api/transactions-by-user?userID=1").asJson();
+        assertEquals(res.getStatus(),200);
+    }
+
+}
