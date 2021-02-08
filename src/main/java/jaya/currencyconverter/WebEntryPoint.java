48934ef9f4cfec2dd8e8d7fcc4f0bae25309d474
Sereno Mendes Nascimento <sereno.desenvolvimento@gmail.com>
2021-02-08 03:48:29 -0300

    Changing the initialization to listening any ip

--- src/main/java/jaya/currencyconverter/WebEntryPoint.java
+++ src/main/java/jaya/currencyconverter/WebEntryPoint.java
@@ -7,9 +7,6 @@ import javax.inject.Singleton;
 
 import com.google.inject.Inject;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.util.PathWatcher.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,16 +38,7 @@ public class WebEntryPoint {
     
     public void boot(){
         bindRoutes();
-
-        Server server = new Server();
-        ServerConnector connector = new ServerConnector(server);
-        connector.setHost("0.0.0.0");
-        connector.setPort(5000);
-
-        this.app.create(conf -> {
-            conf.server(()->server);
-        })
-        .start();
+        this.app.start("0.0.0.0",5000);
     }
 
 }
