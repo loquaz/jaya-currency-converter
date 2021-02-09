5ac5659158d6bff9cfd85b053649fbe33c370502
Sereno Mendes Nascimento <sereno.desenvolvimento@gmail.com>
2021-02-08 23:39:33 -0300

    Added OpenAPI documentation

--- src/main/java/jaya/currencyconverter/controller/UserController.java
+++ src/main/java/jaya/currencyconverter/controller/UserController.java
@@ -6,6 +6,13 @@ import javax.inject.Singleton;
 import org.slf4j.Logger;
 
 import io.javalin.http.Context;
+import io.javalin.plugin.openapi.annotations.HttpMethod;
+import io.javalin.plugin.openapi.annotations.OpenApi;
+import io.javalin.plugin.openapi.annotations.OpenApiContent;
+import io.javalin.plugin.openapi.annotations.OpenApiParam;
+import io.javalin.plugin.openapi.annotations.OpenApiRequestBody;
+import io.javalin.plugin.openapi.annotations.OpenApiResponse;
+
 import jaya.currencyconverter.config.module.UserServiceAnnotations.UserControllerLogger;
 import jaya.currencyconverter.dto.HttpResponseDTO;
 import jaya.currencyconverter.dto.UserDTO;
@@ -34,6 +41,19 @@ public class UserController {
      * - username
      * - password
      */
+    @OpenApi(
+        path = "/api/user/signup",
+        method = HttpMethod.POST,
+        summary = "sign up an user",
+        pathParams = {
+            @OpenApiParam(name = "username", type = String.class, description = "username"),
+            @OpenApiParam(name = "password", type = String.class, description = "password"),
+        },
+        requestBody = @OpenApiRequestBody( content = @OpenApiContent( from = User.class ) ),
+        responses = {
+            @OpenApiResponse(status = "200", content = @OpenApiContent( from = HttpResponseDTO.class ))
+        }
+    )
     public void signup(Context ctx)  {
  
         try{
